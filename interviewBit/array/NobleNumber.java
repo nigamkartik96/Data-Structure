e503a0cf84fdea752f726036f873ac6fd33f38d0
nigamkartik96 <nigamkartik962@gmail.com>
2019-07-21 00:11:34 +0530

    master: Improvements in Noble Number

--- interviewBit/array/NobleNumber.java
+++ interviewBit/array/NobleNumber.java
@@ -4,39 +4,31 @@
  */
 package array;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class NobleNumber {
+    static boolean positiveFlag = true, existFlag = false;
+    static int currentLen = 0;
+
     public int solve(List<Integer> A) {
         Collections.sort(A);
-        // System.out.println(A);
-        boolean flag = true;
+
         LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
         for (int a : A) {
             if (!map.containsKey(a)) map.put(a, 1);
             else map.put(a, map.get(a) + 1);
-            if (a > 0) flag = false;
+            positiveFlag = a > 0;
         }
-        int n = A.size(), currentLen = 0;
-        /*for(int i = 0; i < arr.length; i++) {
-            currentLen += arr[i];
-            // System.out.println(currentLen + " " + n + " " + (n - currentLen) + " " + i);
-            if(arr[i] != 0 && (n - currentLen) == i) return 1;
-        }*/
-
-        /*map.forEach((k, v) -> {
+        int n = A.size();
+        map.forEach((k, v) -> {
             currentLen += v;
-            System.out.println(currentLen + " " + n + " " + (n - currentLen) + " " + k);
-            if(v != 0 && (n - currentLen) == k) flag = 1;
-        });*/
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int k = entry.getKey(), v = entry.getValue();
-            currentLen += v;
-            // System.out.println(currentLen + " " + n + " " + (n - currentLen) + " " + k);
-            if (v != 0 && (n - currentLen) == k) return 1;
-            if (k < 0 && v != 0 && (n - currentLen) == Math.abs(k) && flag) return 1;
-        }
-        return -1;
+            if (v != 0 && (n - currentLen) == k) existFlag = true;
+            if (k < 0 && v != 0 && (n - currentLen) == Math.abs(k) && positiveFlag) existFlag = true;
+        });
+        return existFlag ? 1 : -1;
     }
 
     public static void main(String args[]) {
