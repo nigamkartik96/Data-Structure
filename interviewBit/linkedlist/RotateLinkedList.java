43afc1ab00221bf9fe9a535dd5eb1cac1c422517
nigamkartik96 <nigamkartik962@gmail.com>
2019-07-28 11:46:29 +0530

    master: Reverse linked list between two

--- interviewBit/linkedlist/RotateLinkedList.java
+++ interviewBit/linkedlist/RotateLinkedList.java
@@ -1,3 +1,16 @@
+/**
+ * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+ * <p>
+ * For example:
+ * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+ * <p>
+ * return 1->4->3->2->5->NULL.
+ * <p>
+ * Note:
+ * Given m, n satisfy the following condition:
+ * 1 ≤ m ≤ n ≤ length of list. Note 2:
+ * Usually the version often seen in the interviews is reversing the whole linked list which is obviously an easier version of this question.
+ */
 package linkedlist;
 
 import java.util.Stack;
