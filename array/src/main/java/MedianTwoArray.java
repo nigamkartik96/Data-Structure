9f8e6f5083b76c6d5448888c027ead73224eae8f
nigamkartik96 <nigamkartik962@gmail.com>
2019-09-23 22:52:29 +0530

    master: Changes in Find the median

--- array/src/main/java/MedianTwoArray.java
+++ array/src/main/java/MedianTwoArray.java
@@ -1,51 +1,48 @@
 public class MedianTwoArray {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums2.length == 0) {
-            if (nums1.length % 2 == 0)
-                return (nums1[nums1.length / 2] + nums1[nums1.length / 2 - 1]) / 2.0;
-            else {
-                return nums1[nums1.length / 2];
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) {
+            int[] temp = A;
+            A = B;
+            B = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j - 1] > A[i]) {
+                iMin = i + 1; // i is too small
+            } else if (i > iMin && A[i - 1] > B[j]) {
+                iMax = i - 1; // i is too big
+            } else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
             }
         }
-
-        if (nums1.length == 0) {
-            if (nums2.length % 2 == 0)
-                return (nums2[nums2.length / 2] + nums2[nums2.length / 2 - 1]) / 2.0;
-            else {
-                return nums2[nums2.length / 2];
-            }
-        }
-        int m = Math.max(nums1.length, nums2.length);
-        int n = Math.min(nums1.length, nums2.length);
-
-        int minIndex = 0, maxIndex = n, i = 0, j = 0, median = 0;
-        while (minIndex < maxIndex) {
-            i = (minIndex + maxIndex) / 2;
-            j = (n + m + 1) / 2 - i;
-
-            if (i < n && j > 0 && nums2[j - 1] > nums1[i])
-                minIndex = i + 1;
-            else if (i > 0 && j < m && nums2[j] < nums1[i - 1])
-                maxIndex = i - 1;
-            else {
-                if (i == 0)
-                    median = nums1[j - 1];
-                else if (j == 0)
-                    median = nums2[i - 1];
-                else
-                    median = Math.max(nums1[i - 1], nums2[j - 1]);
-                break;
-            }
-        }
-        if ((n + m) % 2 == 1)
-            return (double) median;
-
-        if (i == n)
-            return (median + nums2[j]) / 2.0;
-
-        if (j == m)
-            return (median + nums1[i]) / 2.0;
-
-        return (median + Math.min(nums1[i], nums2[j])) / 2.0;
+        return 0.0;
     }
 }
