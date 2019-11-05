b4a093779e212df6b7aa5fa4518ceae4b12b1951
nigamkartik96 <nigamkartik962@gmail.com>
2019-11-05 16:12:56 +0530

    master: Sliding Window changes

--- strings/src/LongestNonRepeating.java
+++ strings/src/LongestNonRepeating.java
@@ -20,6 +20,7 @@
  */
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class LongestNonRepeating {
     public int lengthOfLongestSubstring(String s) {
@@ -47,8 +48,25 @@ public class LongestNonRepeating {
         return max;
     }
 
+    public int lengthOfLongestSubstringSlidingWindow(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0, n = s.length();
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                j++;
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
     public static void main(String args[]) {
         LongestNonRepeating obj = new LongestNonRepeating();
         System.out.println(obj.lengthOfLongestSubstring("abcaabhi"));
+        System.out.println(obj.lengthOfLongestSubstringSlidingWindow("abcaabhi"));
     }
 }
