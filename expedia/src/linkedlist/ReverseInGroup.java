49957975dfda21a8e766c06036b9c25dfdabc480
nigamkartik96 <nigamkartik962@gmail.com>
2022-06-25 19:48:20 +0530

    master: Reverse in group

--- expedia/src/linkedlist/ReverseInGroup.java
+++ expedia/src/linkedlist/ReverseInGroup.java
@@ -3,13 +3,31 @@ package linkedlist;
 public class ReverseInGroup {
 
     private static Node reverseInGroup(int size, Node head) {
-        if (head == null || head.next == null) return head;
+        if (head == null) return null;
 
-        return head;
+        Node current = head;
+        Node prev = null;
+        Node next = null;
+
+        int count = 0;
+
+        while (count < size && current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            count++;
+        }
+
+        if (next != null) {
+            head.next = reverseInGroup(size, next);
+        }
+
+        return prev;
     }
 
     public static void main(String[] args) {
         Node head = Utility.getList();
-        Utility.printList(reverseInGroup(2, head));
+        Utility.printList(reverseInGroup(7, head));
     }
 }
